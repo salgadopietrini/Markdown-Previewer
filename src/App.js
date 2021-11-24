35436e02d4d366e7174ebaaa84df17bc9dde90af
manuelsalgado <salgadopietrini7@gmail.com>
2021-11-24 20:55:57 +0000

    add styles and fix preview

--- src/App.js
+++ src/App.js
@@ -2,54 +2,82 @@ import React, { useState } from "react";
 import marked from "marked";
 
 function TextEditor() {
-  const [plainText, setPlainText] = useState("");
+  const [plainText, setPlainText] = useState(
+    "<h1>This is a h1</h1>" +
+      "<h2>This is a h2</h2>" +
+      "<a href=''>This is a link</a>" +
+      "<p><code>This is an inline code</code></p> " +
+      "<code>This is a code block</code>" +
+      "<ul><li>This is the first element of a list</li><li>This is the second element of a list</li></ul>" +
+      "<blockquote cite=''>This is a blockquote</blockquote>" +
+      "<img src='https://static.thenounproject.com/png/526867-200.png' alt='This is an image'>" +
+      "<b>This text is bolded</b>"
+  );
 
   const handleChange = (event) => {
-    setPlainText(marked(event.target.value));
+    setPlainText(event.target.value);
   };
 
   const rendT = () => {
     return {
-      __html: plainText,
+      __html: marked(plainText),
     };
   };
 
   return (
-    <div>
-      <div>Text Editor</div>
-      <label for="textEditor">Text Editor</label>
-      <textarea
-        id="textEditor"
-        name="textEditor"
-        rows="4"
-        cols="50"
-        onChange={handleChange}
-      />
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-around",
+        margin: "50px",
+      }}
+    >
+      <div style={{ width: "40vw", border: "2px solid" }}>
+        <h1
+          style={{
+            height: "10%",
+            padding: "0",
+            margin: "0",
+            textAlign: "center",
+            paddingTop: "10px",
+          }}
+        >
+          Text Editor
+        </h1>
 
-      <div dangerouslySetInnerHTML={rendT()}></div>
-    </div>
-  );
-}
-
-function RenderedText() {
-  return (
-    <div>
-      <div>Rendered Text</div>
+        <textarea
+          id="editor"
+          name="editor"
+          rows="30"
+          style={{ width: "100%", height: "90%", padding: "0", margin: "0" }}
+          value={plainText}
+          onChange={handleChange}
+        />
+      </div>
+      <div style={{ width: "40vw", border: "2px solid" }}>
+        <h1
+          style={{
+            height: "10%",
+            padding: "0",
+            margin: "0",
+            textAlign: "center",
+            paddingTop: "10px",
+          }}
+        >
+          Text Preview
+        </h1>
+        <div
+          dangerouslySetInnerHTML={rendT()}
+          id="preview"
+          style={{ borderTop: "2px solid", padding: "10px" }}
+        ></div>
+      </div>
     </div>
   );
 }
 
 function App() {
-  return (
-    <div>
-      <div>
-        <TextEditor />
-      </div>
-      <div>
-        <RenderedText />
-      </div>
-    </div>
-  );
+  return <TextEditor />;
 }
 
 export default App;
